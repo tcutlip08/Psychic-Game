4e8cc14c7c2f8c3179045c7abada0e0fad76d9b7
unknown <tcutlip08@gmail.com>
2019-06-18 17:21:11 -0400

    just testing the embeded codes for korn use previous code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,7 +8,7 @@
 
 //to be able to copy and paste bands i want list so i can set up a scenario i was having issues with
 //, "metallica", "nonpoint", "sevendust", "korn", "megadeth", "pantera", "disturbed", "godsmack", "acdc", "slipknot"
-var allBandNames = ["metallica", "nonpoint", "sevendust", "korn", "megadeth", "pantera", "disturbed", "godsmack", "acdc", "slipknot"];
+var allBandNames = ["korn"];
 var bandName = allBandNames[Math.floor(Math.random() * allBandNames.length)];
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var alreadyGuessed = [];
